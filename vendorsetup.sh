7f0ac26d3d6c8a2528f516f9b89811416de0dd1b
SEBASTIAN DE JESÚS CASTRO MARTÍNEZ <castromartinezbastiandejesus@gmail.com>
2026-07-26 16:33:36 -0600

    Actualizar vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,90 +1,125 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021-2023 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
+name: TWRP
 
-FDEVICE="gta9p"
-#set -o xtrace
+on:
+  push:
+    branches:
+      - 'twrp-12.1'
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
+    paths:
+      - 'version.mk'
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
+jobs:
+  build:
+    name: Build TWRP by ${{ github.actor }}
+    runs-on: ubuntu-20.04
+    if: github.event.repository.owner.id == github.event.sender.id
+    permissions:
+      contents: write
+    steps:
+    - name: Check Out
+      uses: actions/checkout@v4
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    - name: Cleanup
+      uses: rokibhasansagar/slimhub_actions@main
 
-# Initial Info
-    export ALLOW_MISSING_DEPENDENCIES=true
-    export LC_ALL="C"
-# Maintaining Info
-    export OF_MAINTAINER=Crypton
-    export FOX_VERSION="R11.1"
-    export FOX_BUILD_TYPE=beta
-  	
-# Device Info
-    export FOX_ARCH=arm64
-    export FOX_VARIANT="12.1"
-    export TARGET_DEVICE_ALT="gta9p"
-  
-# Funtions
-    export FOX_REPLACE_BUSYBOX_PS=1
-    export FOX_REPLACE_TOOLBOX_GETPROP=1
-    export FOX_USE_TAR_BINARY=1
-    export FOX_USE_SED_BINARY=1
-    export FOX_USE_BASH_SHELL=1
-    export FOX_ASH_IS_BASH=1
-    export FOX_USE_GREP_BINARY=1
-    export FOX_USE_XZ_UTILS=1
-    export FOX_USE_NANO_EDITOR=1
-    export OF_ENABLE_LPTOOLS=1
-    export FOX_DELETE_AROMAFM=1
-  
-# Display Settings
-    export OF_HIDE_NOTCH=1
-    export OF_CLOCK_POS=1
-    export OF_ALLOW_DISABLE_NAVBAR=0
-    export OF_USE_GREEN_LED=0
-    export OF_SCREEN_H=2400
-  
-# Other Patches
-    export OF_NO_RELOAD_AFTER_DECRYPTION=1
-    export OF_FBE_METADATA_MOUNT_IGNORE=1
-    export OF_PATCH_AVB20=1
-    export OF_NO_SPLASH_CHANGE=1
-    export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
-    export OF_DEFAULT_KEYMASTER_VERSION=4.0
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+    - name: Set Swap Space
+      uses: pierotofy/set-swap-space@master
+      with:
+        swap-size-gb: 24
 
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
-fi
-#
+    - name: Setup ccache
+      uses: hendrikmuhs/ccache-action@v1.2
+      with:
+        max-size: "10G"
+
+    - name: Prepare the environment
+      run: |
+        sudo apt update
+        sudo apt -y upgrade
+        sudo apt -y install gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libtinfo5 libgflags-dev
+        sudo add-apt-repository universe
+        sudo apt -y install libncurses5
+
+    - name: Install OpenJDK
+      uses: actions/setup-java@v4
+      with:
+        distribution: 'zulu'
+        java-version: '8'
+
+    - name: Install Git-Repo
+      run: |
+        mkdir ~/bin
+        curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+        chmod a+x ~/bin/repo
+        sudo ln -sf ~/bin/repo /usr/bin/repo
+
+    - name: Initialize Repo
+      run: |
+        mkdir twrp
+        cd twrp
+        git config --global user.name "castromartinezbastiandejesus-arch"
+        git config --global user.email "castromartinezbastiandejesus@gmail.com"
+        repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1
+        repo sync -j$(nproc --all) --force-sync
+
+    - name: Clone Device Tree
+      run: |
+        cd twrp
+        git clone https://github.com/cd-Crypton/custom_recovery_tree_samsung_gta9p.git -b twrp-12.1 ./device/samsung/gta9p
+        cd device/samsung/gta9p
+        echo "COMMIT_ID=$(git rev-parse HEAD)" >> $GITHUB_ENV
+        echo "VERSION=$(cat version.mk | grep '^TW_DEVICE_VERSION' | awk '{print $3}')" >> $GITHUB_ENV
+        echo "COMMIT_ID2=$(git log --pretty=format:"%H" --grep="Unofficial build release" | sed -n '2p')" >> $GITHUB_ENV
+
+    - name: Building Image
+      run: |
+        echo "BUILD_DATE=$(date +%Y%m%d)" >> $GITHUB_ENV
+        cd twrp
+        export USE_CCACHE=1
+        export CCACHE_EXEC=/usr/bin/ccache
+        sed -i 's/return sandboxConfig\.working/return false/g' build/soong/ui/build/sandbox_linux.go
+        source build/envsetup.sh
+        export ALLOW_MISSING_DEPENDENCIES=true
+        lunch twrp_gta9p-eng && make clean && mka recoveryimage
+        echo "Done building your recovery.img"
+        cd out/target/product/gta9p
+        echo "Creating tar archive for ODIN flashing..."
+        tar -cvf recovery.tar recovery.img
+        echo "Done, download it and flash."
+        
+
+    - name: Set Body
+      run: |
+        echo -e \
+        "
+        ## Unofficial Build for Samsung Galaxy Tab A9+ 5G - SM-X216B (gta9p)
+        _Note: Flash at your own risk. This build were still in the process of testing._
+        ### Resources
+        * [TWRP Manifest](https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp/tree/twrp-12.1)
+        * [Device Tree](https://github.com/cd-Crypton/custom_recovery_tree_samsung_gta9p/tree/twrp-12.1)
+        
+        Build Device: gta9p
+        Build Date: ${{ env.BUILD_DATE }}
+        Build Release: Unofficial Beta
+        Build Branch: 12.1 (v3.7.1)
+        Compatibility: Android 12+
+        Custom Version: ${{ env.VERSION }}
+        
+        ### What's new?
+        * To be edited...
+        * [Full Changelog](https://github.com/cd-Crypton/custom_recovery_tree_samsung_gta9p/compare/${{ env.COMMIT_ID2 }}...${{ env.COMMIT_ID }})
+
+        ### Trigger Event
+        _This build were compiled by triggering branch twrp-12.1 of current tree, triggered by a push event in [version makefile](https://github.com/cd-Crypton/custom_recovery_tree_samsung_gta9p/blob/twrp-12.1/version.mk)._
+        * [Last Commit](https://github.com/cd-Crypton/custom_recovery_tree_samsung_gta9p/commit/${{ env.COMMIT_ID }})
+        " > changelog.txt
+
+    - name: Upload to Release
+      uses: softprops/action-gh-release@v2
+      with:
+        files: |
+          twrp/out/target/product/gta9p/recovery.img
+          twrp/out/target/product/gta9p/recovery.tar
+        name: TWRP Release Build for Samsung Galaxy Tab A9+ 5G - SM-X216B (gta9p) // ${{ env.BUILD_DATE }}
+        tag_name: ${{ github.run_id }}
+        body_path: changelog.txt
